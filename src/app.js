0e3d69ccc99ba40d954b1ec7f41b8665b25963ed
lisandromariano <145061399+lisandromariano@users.noreply.github.com>
2023-11-29 16:01:36 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numeros = [
+    "As",
+    "Rey",
+    "Reina",
+    "J",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+  ];
+  let palos = ["♦", "♥", "♠", "♣"];
+
+  let seleccionDeNumeros = numeros[Math.floor(Math.random() * numeros.length)];
+  let seleccionDePalos = palos[Math.floor(Math.random() * palos.length)];
+
+  document.getElementById("PaloArriba").innerHTML = seleccionDePalos;
+
+  document.getElementById("NumeroCentro").innerHTML = seleccionDeNumeros;
+
+  document.getElementById("PaloAbajo").innerHTML = seleccionDePalos;
+
+  if (seleccionDePalos == "♦" || seleccionDePalos == "♥") {
+    document.getElementById("PaloArriba").className = "card-text text-danger";
+    document.getElementById("PaloAbajo").className = "card-text text-danger";
+  } else {
+    document.getElementById("PaloArriba").className = "card-text text-dark";
+    document.getElementById("PaloAbajo").className = "card-text text-dark";
+  }
 };
